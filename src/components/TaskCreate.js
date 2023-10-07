723ea449611ed26f0784728a650a97d3c64d0742
atakanhbk <atakanhbk@gmail.com>
2023-10-07 14:22:17 +0300

    Handle Change Added

--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,12 +1,32 @@
+import { useState } from "react";
+
 function TaskCreate() {
+  const [title, setTitle] = useState("");
+  const [taskDesc, setTaskDesk] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleTaskChange = (event) => {
+    setTaskDesk(event.target.value); 
+  };
+
+
+
   return (
     <div className="task-create">
       <h3>Please Add Task</h3>
       <form className="task-form">
         <label className="task-label">Title</label>
-        <input className="task-input"/>
+        <input value={title} className="task-input" onChange={handleChange} />
         <label className="task-label">Enter Your Task</label>
-        <textarea className="task-input" rows={5}/>
+        <textarea
+          value={taskDesc}
+          className="task-input"
+          onChange={handleTaskChange}
+          rows={5}
+        />
         <button className="task-button">Create</button>
       </form>
     </div>
