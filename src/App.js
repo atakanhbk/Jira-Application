eda0b3a74b3e6357c6c5568017b4015a340e9fa4
atakanhbk <atakanhbk@gmail.com>
2023-10-07 14:10:06 +0300

    Created Task Create Part

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import TaskCreate from "./components/TaskCreate";
 import TaskList from "./components/TaskList";
 
 function App() {
-  return <div className="App">
-    <TaskCreate/>
-    <h1>Görevler</h1>
-    <TaskList/>
-  </div>;
+  return (
+    <div className="App">
+      <TaskCreate />
+      <h1>Görevler</h1>
+      <TaskList />
+    </div>
+  );
 }
 
 export default App;
--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,7 +1,16 @@
 function TaskCreate() {
-    return <div>
-
-    </div>;
+  return (
+    <div className="task-create">
+      <h3>Please Add Task</h3>
+      <form className="task-form">
+        <label className="task-label">Title</label>
+        <input className="task-input"/>
+        <label className="task-label">Enter Your Task</label>
+        <textarea className="task-input" rows={5}/>
+        <button className="task-button">Create</button>
+      </form>
+    </div>
+  );
 }
 
 export default TaskCreate;
