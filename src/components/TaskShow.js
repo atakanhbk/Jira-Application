cd64e3fcd5dab36596c038ae0288422bcd03a0dc
atakanhbk <atakanhbk@gmail.com>
2023-10-08 18:19:41 +0300

    Fixed CSS

--- src/components/TaskShow.js
+++ src/components/TaskShow.js
@@ -2,14 +2,14 @@ function TaskShow({ task }) {
   console.log(task);
   return (
     <div className="task-show">
-      <h3>Your Task</h3>
+      <h3 className="task-title">Your Task</h3>
       <p>{task.title}</p>
-      <h3>Things To Do</h3>
+      <h3 className="task-title">Things To Do</h3>
       <p>{task.taskDesc}</p>
 
       <div>
-        <button>Delete</button>
-        <button>Update</button>
+        <button className="task-delete">Delete</button>
+        <button className="task-edit">Edit</button>
       </div>
     </div>
   );
