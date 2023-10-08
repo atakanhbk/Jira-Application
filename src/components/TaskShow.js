be109f3c2d0c2b00ce1509ba77e476fca5e3db60
atakanhbk <atakanhbk@gmail.com>
2023-10-08 18:14:01 +0300

    Add CSS

--- src/components/TaskShow.js
+++ src/components/TaskShow.js
@@ -1,11 +1,18 @@
-function TaskShow({task}) {
-    console.log(task);
-    return <div>
-       <h3>Your Task</h3>
-       <p>{task.title}</p>
-       <h3>Things To Do</h3>
-       <p>{task.taskDesc}</p>
-    </div>;
+function TaskShow({ task }) {
+  console.log(task);
+  return (
+    <div className="task-show">
+      <h3>Your Task</h3>
+      <p>{task.title}</p>
+      <h3>Things To Do</h3>
+      <p>{task.taskDesc}</p>
+
+      <div>
+        <button>Delete</button>
+        <button>Update</button>
+      </div>
+    </div>
+  );
 }
 
 export default TaskShow;
